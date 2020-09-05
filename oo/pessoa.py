632a50d1b4430138757739027a7d247e2bafbd55
rlemos37 <lemos_007_@hotmail.com>
2020-09-05 10:44:40 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:  # A classe funciona como uma forma de gelo
-    def __init__(self, nome=None, idade=38):
+    def __init__(self,*filhos, nome=None, idade=38):
         self.idade = idade
         self.nome = nome
+        self.filhos= list(filhos)
 
     def cumprimentar(self):  # para criar tipos personalizados
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Maria')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome='Rone'
-    print(p.nome)
-    print(p.idade)
+    rone = Pessoa(nome='Rone')
+    maria = Pessoa(rone,nome='Maria')
+    print(Pessoa.cumprimentar(maria))
+    print(id(maria))
+    print(maria.cumprimentar())
+    print(maria.nome)
+    print(maria.idade)
+    for filho in maria.filhos:
+        print(filho.nome)
