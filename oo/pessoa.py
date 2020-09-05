4af5cd6792d9cea71fa52a29e8febd1fee96406c
rlemos37 <lemos_007_@hotmail.com>
2020-09-05 10:14:24 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
-class Pessoa:#A classe funciona como uma forma de gelo
-     def cumprimentar(self):           # para criar tipos personalizados
-         return f'Olá {id(self)}'
+class Pessoa:  # A classe funciona como uma forma de gelo
+    def __init__(self, nome=None, idade=38):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):  # para criar tipos personalizados
+        return f'Olá {id(self)}'
+
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Maria')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome='Rone'
+    print(p.nome)
+    print(p.idade)
